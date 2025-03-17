293b9d2c25af3719ec960ac14bd6b9028b055579
Hiddev666 <officehiddev@gmail.com>
2025-03-17 20:11:59 +0700

    feat: completing login route

--- src/routers/authRouter.mjs
+++ src/routers/authRouter.mjs
@@ -3,6 +3,7 @@ import { responseMiddleware } from "../middlewares/responseMiddleware.mjs"
 import User from "../database/models/userModel.mjs"
 import CryptoJS from "crypto-js"
 import dotenv from "dotenv"
+import jwt from "jsonwebtoken"
 
 dotenv.config()
 const authRouter = Router()
@@ -10,17 +11,30 @@ authRouter.use(responseMiddleware)
 
 authRouter.post("/login", async (req, res) => {
     try {
-        const { body: { phone, password } } = req
-        const user = await User.find({ phone })
+        const { body } = req
+        const user = await User.findOne({ phone: body.phone })
 
-        // const decryptedPassword = CryptoJS.AES.decrypt(
-        //     user.password,
-        //     process.env.CRYPTO_KEY
-        // ).toString(CryptoJS.enc.Utf8)
-
-        res.send({
-            data: user
+        const decryptedPassword = CryptoJS.AES.decrypt(
+            user.password,
+            process.env.CRYPTO_KEY
+        ).toString(CryptoJS.enc.Utf8)
+        if (decryptedPassword != body.password) return res.status(400).send({
+            message: "Bad Request",
+            error: "Your Enter Invalid Password"
         })
+
+        const token = jwt.sign(
+            {
+                userId: user._id,
+                role: user.role
+            },
+            process.env.JWT_KEY,
+            { expiresIn: "3d" }
+        )
+
+        const { password, __v, ...credentials } = user._doc
+
+        res.cookie("token", token).success(200, "Login Success", { ...credentials, token })
     } catch (err) {
         console.log(err.message)
         res.fail("Login Failed", err)
